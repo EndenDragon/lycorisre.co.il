058c4c12e5b51459994364536fcf5167998fc25f
Jeremy Zhang <jkzhang@uw.edu>
2022-09-26 05:03:40 -0700

    Tweet handler

--- lycoris/static/script.js
+++ lycoris/static/script.js
@@ -6,6 +6,11 @@
     let hoveringVideo = false;
     let videoLastTime = 0;
     let hoverVideoTimer = null;
+    let loadingNewTweets = false;
+    let totalTweets = 0;
+    let tweetsOnScreen = 0;
+    let tweets = [];
+    let tweetPosition = 0;
 
     window.addEventListener("load", init);
 
@@ -17,12 +22,15 @@
 
         document.getElementById("video").addEventListener("mouseover", videoHover);
         document.getElementById("video").addEventListener("mouseleave", videoHoverStop);
+        window.addEventListener("scroll", handleScroll);
+
+        getTweets();
     }
 
     function videoHover() {
         hoveringVideo = true;
         clearTimeout(hoverVideoTimer);
-        hoverVideoTimer = setTimeout(startVideo, 1000);
+        hoverVideoTimer = setTimeout(startVideo, 500);
     }
 
     function videoHoverStop() {
@@ -69,5 +77,97 @@
         req.send();
     }
 
+    function elementInViewport(el) {
+        let top = el.offsetTop;
+        let left = el.offsetLeft;
+        let width = el.offsetWidth;
+        let height = el.offsetHeight;
+      
+        while(el.offsetParent) {
+            el = el.offsetParent;
+            top += el.offsetTop;
+            left += el.offsetLeft;
+        }
+      
+        return (
+            top >= window.pageYOffset &&
+            left >= window.pageXOffset &&
+            (top + height) <= (window.pageYOffset + window.innerHeight) &&
+            (left + width) <= (window.pageXOffset + window.innerWidth)
+        );
+    }
 
+    function shuffle(array) {
+        let currentIndex = array.length,  randomIndex;
+
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
+            // Pick a remaining element.
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+        
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+        }
+        return array;
+    }
+
+    function getTweets() {
+        fetch("static/tweets.txt")
+            .then(loadTweets);
+    }
+
+    async function loadTweets(response) {
+        let responseTweets = await response.text();
+        let tweetIds = responseTweets.trim().split(/\r?\n/);
+        let featured = [];
+        for (let i = 0; i < 3; i++) {
+            featured.push(tweetIds.shift());
+        }
+        shuffle(tweetIds);
+        shuffle(featured);
+        while (featured.length) {
+            tweetIds.splice(Math.floor(Math.random() * 25), 0, featured.pop());
+        }
+        tweets = tweetIds;
+    }
+
+    function handleScroll() {
+        if (loadingNewTweets || tweets.length == 0 || !elementInViewport(document.getElementById("loading"))) {
+            return;
+        }
+        loadingNewTweets = true;
+
+        let end = Math.min(tweets.length, tweetPosition + 12);
+        for (let i = tweetPosition; i < end; i++) {
+            insertTweet(tweets[i]);
+            totalTweets++;
+            tweetPosition++;
+        }
+    }
+
+    function insertTweet(tweetId) {
+        let container = document.createElement("div");
+        container.classList.add("temp");
+        document.getElementById("tweets").appendChild(container);
+        twttr.widgets.createTweet(
+            tweetId,
+            container,
+            {
+                theme: "light",
+                dnt: true
+            }
+        ).then(function (el) {
+            container.classList.remove("temp");
+            tweetsOnScreen++;
+            if (totalTweets == tweetsOnScreen) {
+                loadingNewTweets = false;
+                if (Math.random() < 0.5) {
+                    document.getElementById("loading").src = "static/img/chisatomote.png";
+                } else {
+                    document.getElementById("loading").src = "static/img/takinamote.png";
+                }
+            }
+        });
+    }
 })();
